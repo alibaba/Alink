74b982bde188d250ce3bc9c64c0f16e4137e4b69
shaomengwang <shaomeng.wang.w@gmail.com>
2021-02-05 12:18:12 +0800

    Remove the filter in onehot train op when the threshold less than 1. see #112.

--- core/src/main/java/com/alibaba/alink/operator/batch/feature/OneHotTrainBatchOp.java
+++ core/src/main/java/com/alibaba/alink/operator/batch/feature/OneHotTrainBatchOp.java
@@ -86,18 +86,22 @@ public final class OneHotTrainBatchOp extends BatchOperator <OneHotTrainBatchOp>
 		boolean enableElse = isEnableElse(thresholdArray);
 
 		DataSet <Row> inputRows = in.select(selectedColNames).getDataSet();
-		DataSet <Tuple3 <Integer, String, Long>> countTokens = StringIndexerUtil.countTokens(inputRows, true)
-			.filter(new FilterFunction <Tuple3 <Integer, String, Long>>() {
-				private static final long serialVersionUID = -8219708805787440332L;
+		DataSet <Tuple3 <Integer, String, Long>> countTokens = StringIndexerUtil.countTokens(inputRows, true);
 
-				@Override
-				public boolean filter(Tuple3 <Integer, String, Long> value) {
-					return value.f2 >= thresholdArray[value.f0];
-				}
-			});
+		if (enableElse) {
+			countTokens = countTokens
+				.filter(new FilterFunction <Tuple3 <Integer, String, Long>>() {
+					private static final long serialVersionUID = -8219708805787440332L;
+
+					@Override
+					public boolean filter(Tuple3 <Integer, String, Long> value) {
+						return value.f2 >= thresholdArray[value.f0];
+					}
+				});
+		}
 
 		DataSet <Tuple3 <Integer, String, Long>> indexedToken = StringIndexerUtil
-			.zipWithIndexPerColumn((DataSet) countTokens.project(0, 1))
+			.zipWithIndexPerColumn(countTokens.project(0, 1))
 			.project(1, 2, 0);
 
 		DataSet <Row> values = indexedToken
