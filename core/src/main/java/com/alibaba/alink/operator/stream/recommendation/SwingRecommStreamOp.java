dd987946193f9e5addbacb2cacb7cb5f585b9bf7
shaomengwang <shaomeng.wang.w@gmail.com>
2021-06-11 16:12:51 +0800

    Hotfix on recommendation.

--- core/src/main/java/com/alibaba/alink/operator/stream/recommendation/SwingRecommStreamOp.java
+++ core/src/main/java/com/alibaba/alink/operator/stream/recommendation/SwingRecommStreamOp.java
@@ -1,10 +1,11 @@
 package com.alibaba.alink.operator.stream.recommendation;
 
-import com.alibaba.alink.operator.batch.BatchOperator;
-import com.alibaba.alink.operator.common.recommendation.ItemCfRecommKernel;
-import com.alibaba.alink.operator.common.recommendation.RecommType;
-import com.alibaba.alink.params.recommendation.BaseSimilarItemsRecommParams;
 import org.apache.flink.ml.api.misc.param.Params;
+
+import com.alibaba.alink.operator.batch.BatchOperator;
+import com.alibaba.alink.operator.common.recommendation.RecommType;
+import com.alibaba.alink.operator.common.recommendation.SwingRecommKernel;
+import com.alibaba.alink.params.recommendation.BaseSimilarItemsRecommParams;
 
 public class SwingRecommStreamOp extends BaseRecommStreamOp<SwingRecommStreamOp>
     implements BaseSimilarItemsRecommParams<SwingRecommStreamOp> {
@@ -13,6 +14,6 @@ public class SwingRecommStreamOp extends BaseRecommStreamOp<SwingRecommStreamOp>
     }
 
     public SwingRecommStreamOp(BatchOperator model, Params params) {
-        super(model, ItemCfRecommKernel::new, RecommType.SIMILAR_ITEMS, params);
+        super(model, SwingRecommKernel::new, RecommType.SIMILAR_ITEMS, params);
     }
 }
